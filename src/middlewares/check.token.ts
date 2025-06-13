9d5de535ea93537e5b5dc23b9d63847b585f803d
TanishqPokharia <tppokharia@gmail.com>
2025-06-13 16:54:15 +0530

    feat: added middlewares

---
+++ src/middlewares/check.token.ts
@@ -0,0 +1,44 @@
+import { Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import AuthenticatedRequest from "../models/authenticated.request";
+import logger from "../utils/logger";
+const checkToken = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const auth = req.headers.authorization?.split(" ");
+    if (!auth || auth.at(0) !== "Bearer" || !auth.at(1)) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const tokenString = auth[1];
+    const token = jwt.verify(
+      tokenString,
+      process.env.ACCESS_TOKEN_SECRET as string,
+      { algorithms: ["HS256"] }
+    );
+    if (typeof token === "string") {
+      return res
+        .status(401)
+        .json({ error: "Unauthorized: Invalid token format" });
+    }
+
+    req.token = token.id;
+
+    next();
+  } catch (error) {
+    logger.error(error, "Error verifying token:");
+    if (error instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({ error: "Unauthorized: Invalid token" });
+    }
+    if (error instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ error: "Unauthorized: Token expired" });
+    }
+
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export default checkToken;
